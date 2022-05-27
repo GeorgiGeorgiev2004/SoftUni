6294201f39be450fe28c32116ebe3c7bd16079d3
Georgi <gogotin5@gmail.com>
2022-05-27 19:49:26 +0300

    solved problem 1

--- CSharp-Fundamentals/Homework/DataTypesAndVariables/01. Integer Operations/01. Integer Operations/Program.cs
+++ CSharp-Fundamentals/Homework/DataTypesAndVariables/01. Integer Operations/01. Integer Operations/Program.cs
@@ -6,7 +6,12 @@ namespace _01._Integer_Operations
     {
         static void Main(string[] args)
         {
-            int pepega = 567;
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int sum = (a + b) / c * d;
+            Console.WriteLine(sum);
         }
     }
 }
