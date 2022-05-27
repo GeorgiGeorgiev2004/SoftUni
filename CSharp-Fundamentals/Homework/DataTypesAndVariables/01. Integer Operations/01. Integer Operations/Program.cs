b1056450c1c9c73a08529896a857b8a9463092ab
Georgi <gogotin5@gmail.com>
2022-05-27 19:33:28 +0300

    x

---
+++ CSharp-Fundamentals/Homework/DataTypesAndVariables/01. Integer Operations/01. Integer Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
