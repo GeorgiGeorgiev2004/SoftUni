6cfacc62c8949b47047f0b31e4b179666c5d84a6
Georgi <gogotin5@gmail.com>
2022-05-27 21:25:55 +0300

    solved problem 5 and 6

---
+++ CSharp-Fundamentals/Homework/DataTypesAndVariables/01. Integer Operations/07. Water Overflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kur!!!");
+        }
+    }
+}
