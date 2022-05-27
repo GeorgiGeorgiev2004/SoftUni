1e4413f57dad3f8e3d64a660e100e38f1c70232c
Georgi <gogotin5@gmail.com>
2022-05-27 20:04:24 +0300

    ss

--- CSharp-Fundamentals/Homework/DataTypesAndVariables/01. Integer Operations/3. Elevator/Program.cs
+++ CSharp-Fundamentals/Homework/DataTypesAndVariables/01. Integer Operations/3. Elevator/Program.cs
@@ -6,7 +6,20 @@ namespace _3._Elevator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int trips = 0;
+            while (a>0)
+            {
+                trips++;
+                a -= b;
+                if (b > a && a > 0)
+                {
+                    trips++;
+                    break;
+                }
+            }
+            Console.WriteLine(trips);
         }
     }
 }
