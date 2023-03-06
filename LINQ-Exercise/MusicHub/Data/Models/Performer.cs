e0566f55db579afc0be77bdb39a0b835bc669b03
Georgi Georgiev <gogo_5a@abv.bg>
2023-03-06 14:20:02 +0200

    Did LINQ Exercise

---
+++ LINQ-Exercise/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models;
+
+public class Performer
+{
+    public Performer()
+    {
+        this.PerformerSongs = new HashSet<SongPerformer>();
+    }
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(20)]
+    public string FirstName { get; set; }
+    [Required]
+    [MaxLength(20)]
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public decimal NetWorth { get; set; }
+    public ICollection<SongPerformer> PerformerSongs  { get; set; }
+}
---
+++ LINQ-Exercise/MusicHub/Data/Models/SongPerformer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models;
+
+public class SongPerformer
+{
+    [Required]
+    [ForeignKey(nameof(Song))]
+    public int SongId { get; set; }
+    [Required]
+    public Song Song { get; set; }
+
+
+    [Required]
+    [ForeignKey(nameof(Performer))]
+    public int PerformerId { get; set; }
+    [Required]
+    public Performer Performer { get; set; }
+}
