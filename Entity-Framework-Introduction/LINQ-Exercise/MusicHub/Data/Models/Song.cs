493b4df86527667c67b1994dc8e536be7cf489f6
Georgi Georgiev <gogo_5a@abv.bg>
2023-03-08 17:19:24 +0200

    Moved the LINQ exercise in the correct folder

---
+++ Entity-Framework-Introduction/LINQ-Exercise/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MusicHub.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models;
+
+public class Song
+{
+    public Song()
+    {
+        this.SongPerformers = new HashSet<SongPerformer>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(20)]
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public TimeSpan Duration { get; set; }
+    [Required]
+    public DateTime CreatedOn { get; set; }
+
+
+    [Required]
+    public Genre Genre { get; set; }
+
+    [ForeignKey(nameof(Album))]
+    public int? AlbumId { get; set; }
+    public Album Album { get; set; }
+    [Required]
+    [ForeignKey(nameof(Writer))]
+    public int WriterId { get; set; }
+    public Writer Writer { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    public ICollection<SongPerformer> SongPerformers  { get; set; }
+}
