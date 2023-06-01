53e80b2a53451e7acd9fb8a0a33dc2a4072f07e9
Georgi Georgiev <gogo_5a@abv.bg>
2023-06-01 11:31:46 +0300

    Completely implemented features №3-7

---
+++ Web-Developement/ASP.NET Fundamentals/MyFirstMVCProject/MyFirstMVCProject/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCProject.Models.Product;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel>products = new List<ProductViewModel>() 
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+        public IActionResult All() 
+        {
+            return View(products);
+        }
+        public IActionResult ById(int? id)
+        {
+            var product = products.FirstOrDefault(p=>p.Id==id);
+            if (product == null) 
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
---
+++ Web-Developement/ASP.NET Fundamentals/MyFirstMVCProject/MyFirstMVCProject/Models/Product/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstMVCProject.Models.Product
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
